4156b47ec5e3f874d512a7bd3197bc252d9c9b4e
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-04-10 11:23:50 -0300

    atualizado

---
+++ aula14/index.js
@@ -0,0 +1,13 @@
+
+// IEEE 754-2008 // PADRÃO QUE O JAVASCRIPT SEGUE PARA OS PADRÕES DOS NÚMEROS
+
+/* let num = 10
+console.log(num.toString(2)) */ // Converte em binário 
+
+
+/* let num = 15.578955125547
+
+console.log(num.toFixed(2)) // Deixa um numero em 2 casas decimais */
+
+let num = 10
+console.log(Number.isInteger(num)) // para saber se um número é inteiro, ou ponto flutuante
---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+
+
+/* let num = 9.54578
+let num1 = Math.floor(num) // Arredonda o número para baixo.
+        // Math.ceil(num) // Arredonda o número para cima.
+        // Math.round(num) // Arredonda o número para um mas próximo.
+console.log(num1) */
+
+
+let num = 9
+console.log(num ** (1/2)) // Aqui eu sei a RAIZ quadrada de 9
