8ba56bc84b37e7a49139375d577d45f5db3850c2
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-04-12 14:05:13 -0300

    atualizado

---
+++ aula17/index.js
@@ -0,0 +1,39 @@
+
+
+/* function saudacao(nome) {
+
+    
+    return `Bom dia ${nome}`
+
+}
+
+
+let func = saudacao('Roberto')
+console.log(func) */
+
+/* function soma(x = 1, y =2) {
+
+    let res = x + y
+
+    return res
+}
+
+let res = soma()
+console.log(res) */
+
+
+
+///////////////////////////////////////////////////////////////////////
+
+/* let raiz = function (n) {
+
+return n ** 0.5
+
+};
+
+console.log(raiz(16)) */
+
+
+let raiz = n => n** 0.5 // A funçaõ em cima está simplificada, nesse codigo, ARROW
+
+console.log(raiz(25))
