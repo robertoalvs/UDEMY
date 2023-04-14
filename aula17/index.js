5842f991523fede1070b23d40de5803b4f23c64e
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-04-14 14:12:59 -0300

    atualizado

--- aula17/index.js
+++ aula17/index.js
@@ -34,6 +34,14 @@ return n ** 0.5
 console.log(raiz(16)) */
 
 
-let raiz = n => n** 0.5 // A funçaõ em cima está simplificada, nesse codigo, ARROW
+/* let raiz = n => n** 0.5 // A funçaõ em cima está simplificada, nesse codigo, ARROW
 
-console.log(raiz(25))
+console.log(raiz(25)) */
+
+
+function distance(x1, y1, x2, y2) {
+
+    let dx = x2 - x1
+    let dy = y2 - y1
+    return Math.sqrt(dx*dx + dy*dy)
+}   
